1355b0f938811a3851d68b9a5fbcecfcb2af7976
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-10-10 22:29:38 +0900

    [Fix] fix to use route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from './component/Common/Header';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from './component/Search';
 
 function App() {
   return (
