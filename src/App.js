106a23a000e6c929549dc6f9c7111298be69bfba
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-10-10 22:29:38 +0900

    [Fix] fix to use route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from './component/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <h1>Hi</h1>
+      <Routes>
+        <Route exact path="/" element={<Search />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/component/Search.js
+++ src/component/Search.js
@@ -1,1 +1,7 @@
-hi
+import React from "react";
+
+function Search () {
+    <h1>h1</h1>
+}
+
+export default Search;
