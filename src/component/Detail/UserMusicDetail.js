d6cb579e5d11e579721c8c02a6f7ce0c1ce7c2e5
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-10-19 15:14:12 +0900

    [Add] add show detail by modal

---
+++ src/component/Detail/UserMusicDetail.js
@@ -0,0 +1,27 @@
+import { Avatar } from 'antd'; // Spin은 로딩 애니메이션을 위한 컴포넌트
+
+const AlbumDetails = ({ album }) => {
+
+    if (!album) return null; // trackInfo가 없으면 null 반환
+
+    // 곡 시간 초로 맞추기
+    const durationInMinutes = Math.floor(album.duration / 60);
+    const durationInSeconds = Math.floor((album.duration % 60));
+    const formattedDuration = `${durationInMinutes}:${durationInSeconds < 10 ? '0' : ''}${durationInSeconds}`;
+
+    const releaseDate = album.duration || 'No release date available.';
+    const details = album.memo || 'No memo';
+
+    return (
+        <div>
+            <Avatar src={album.image} size={128} className="album-image" />
+            <h2>{album.name}</h2>
+            <p><strong>Artist:</strong> {album.artist}</p>
+            <p><strong>Release Date:</strong> {releaseDate}</p> {/* 발매일 정보임 */}
+            <p><strong>Duration:</strong> {formattedDuration}</p> {/* 노래 재생 시간 */}
+            <p><strong>Album Details:</strong> {details}</p> {/* 세부정보인데 <a>이거 빼고 */}
+        </div>
+    );
+};
+
+export default AlbumDetails;
